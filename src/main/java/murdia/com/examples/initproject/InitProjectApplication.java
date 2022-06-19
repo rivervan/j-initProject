c83a2e6418d21588e627dbfac2f2d4570bb75de9
r.ivanriveraislas <rivervan02@gmail.com>
2022-06-18 22:53:16 -0500

    Initial commit

---
+++ src/main/java/murdia/com/examples/initproject/InitProjectApplication.java
@@ -0,0 +1,13 @@
+package murdia.com.examples.initproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/murdia/com/examples/initproject/InitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package murdia.com.examples.initproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
